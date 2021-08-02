14ef36f7772b148d0c72f5d285bb69c5cbbd695a
github.com <amakutsevich@bk.ru>
2021-08-02 13:57:07 +0900

    checkstyle

--- src/main/java/ru/job4j/App.java
+++ src/main/java/ru/job4j/App.java
@@ -2,12 +2,9 @@ package ru.job4j;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
