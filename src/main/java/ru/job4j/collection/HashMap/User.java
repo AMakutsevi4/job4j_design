f88cfc00e837fee655ccdc61379921ffa2c2aadd
github.com <amakutsevich@bk.ru>
2021-08-22 21:18:16 +0900

    1. Создать модель User [#999]

---
+++ src/main/java/ru/job4j/collection/HashMap/User.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection.HashMap;
+
+import java.util.Calendar;
+
+public class User {
+    private final String name;
+    private final int children;
+    private final Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+}
