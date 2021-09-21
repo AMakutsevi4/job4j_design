54a9fd27a6b68de232defed9513cdd3261a7ff10
AMakutsevi4 <amakutsevich@bk.ru>
2021-09-21 14:56:49 +0900

    2. Статистика по коллекции. [#45889]

---
+++ src/main/java/ru/job4j/question/Analize.java
@@ -0,0 +1,80 @@
+package ru.job4j.question;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+public class Analize {
+
+    public static Info diff(Set<User> previous, Set<User> current) {
+        Info info = new Info(0, 0, 0);
+        Map<Integer, String> table = new HashMap<>();
+        for (User prev : previous) {
+            table.put(prev.getId(), prev.getName());
+        }
+        for (User curr : current) {
+            if (table.containsKey(curr.getId())) {
+                if (!Objects.equals(curr.getName(), table.get(curr.getId()))) {
+                    info.changed++;
+                }
+            } else {
+                info.added++;
+            }
+        }
+        info.deleted = previous.size() + info.getAdded() - current.size();
+        return info;
+    }
+
+    public static class User {
+        private final int id;
+        private final String name;
+
+        public int getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public User(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, name);
+        }
+    }
+
+    public static class Info {
+        private int added;
+        private int changed;
+        private int deleted;
+
+        public int getAdded() {
+            return added;
+        }
+
+        public Info(int added, int changed, int deleted) {
+            this.added = added;
+            this.changed = changed;
+            this.deleted = deleted;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Info info = (Info) o;
+            return added == info.added && changed == info.changed && deleted == info.deleted;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(added, changed, deleted);
+        }
+    }
+}
---
+++ src/test/java/ru/job4j/question/AnalizeTest.java
@@ -0,0 +1,76 @@
+package ru.job4j.question;
+
+import org.junit.Test;
+import java.util.Set;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class AnalizeTest {
+
+    @Test
+    public void whenNotChanged() {
+        Analize.User u1 = new Analize.User(1, "A");
+        Analize.User u2 = new Analize.User(2, "B");
+        Analize.User u3 = new Analize.User(3, "C");
+        Set<Analize.User> previous = Set.of(u1, u2, u3);
+        Set<Analize.User> current = Set.of(u1, u2, u3);
+        assertThat(
+                Analize.diff(previous, current),
+                is(new Analize.Info(0, 0, 0))
+        );
+    }
+
+    @Test
+    public void whenOneChanged() {
+        Analize.User u1 = new Analize.User(1, "A");
+        Analize.User u2 = new Analize.User(2, "B");
+        Analize.User u3 = new Analize.User(3, "C");
+        Set<Analize.User> previous = Set.of(u1, u2, u3);
+        Set<Analize.User> current = Set.of(u1, new Analize.User(2, "BB"), u3);
+        assertThat(
+                Analize.diff(previous, current),
+                is(new Analize.Info(0, 1, 0))
+        );
+    }
+
+    @Test
+    public void whenOneDeleted() {
+        Analize.User u1 = new Analize.User(1, "A");
+        Analize.User u2 = new Analize.User(2, "B");
+        Analize.User u3 = new Analize.User(3, "C");
+        Set<Analize.User> previous = Set.of(u1, u2, u3);
+        Set<Analize.User> current = Set.of(u1, u3);
+        assertThat(
+                Analize.diff(previous, current),
+                is(new Analize.Info(0, 0, 1))
+        );
+    }
+
+    @Test
+    public void whenOneAdded() {
+        Analize.User u1 = new Analize.User(1, "A");
+        Analize.User u2 = new Analize.User(2, "B");
+        Analize.User u3 = new Analize.User(3, "C");
+        Set<Analize.User> previous = Set.of(u1, u2, u3);
+        Set<Analize.User> current = Set.of(u1, u2, u3, new Analize.User(4, "D"));
+        assertThat(
+                Analize.diff(previous, current),
+                is(new Analize.Info(1, 0, 0))
+        );
+    }
+
+    @Test
+    public void whenAllChanged() {
+        Analize.User u1 = new Analize.User(1, "A");
+        Analize.User u2 = new Analize.User(2, "B");
+        Analize.User u3 = new Analize.User(3, "C");
+        Set<Analize.User> previous = Set.of(u1, u2, u3);
+        Set<Analize.User> current = Set.of(new Analize.User(1, "AA"), u2, new Analize.User(4, "D"));
+        assertThat(
+                Analize.diff(previous, current),
+                is(new Analize.Info(1, 1, 1))
+        );
+    }
+
+}
