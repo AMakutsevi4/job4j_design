d0f63ac7508b620c20adbbc4597cfc585bb5ff39
AMakutsevi4 <amakutsevich@bk.ru>
2021-09-23 14:35:46 +0900

    0.1. FileOutputStream. [#252488 #206862]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -3,13 +3,26 @@ package ru.job4j.io;
 import java.io.FileOutputStream;
 
 public class ResultFile {
-    public static void main(String[] args) {
-        try (FileOutputStream out = new FileOutputStream("result.txt")) {
-            for (byte i = 0; i < 10; i++) {
-                for (byte j = 0; j < 10; j++) {
-                    out.write((((i + 1) * (j + 1)) + System.lineSeparator()).getBytes());
+
+    public static String multiplayer(int size) {
+        StringBuilder rsl = new StringBuilder();
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                rsl.append((i + 1) * (j + 1));
+                if (j + 1 == size) {
+                    rsl.append(System.lineSeparator());
+                } else {
+                    rsl.append("  ");
                 }
             }
+
+        }
+        return rsl.toString();
+    }
+
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            out.write(multiplayer(9).getBytes());
             out.write(System.lineSeparator().getBytes());
             out.write("multiplication table is printed".getBytes());
         } catch (Exception e) {
