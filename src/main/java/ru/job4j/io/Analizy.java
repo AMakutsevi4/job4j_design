329932bc38fe87bbea7d4e1238aea37420971095
AMakutsevi4 <amakutsevich@bk.ru>
2021-09-27 14:24:50 +0900

    2. Анализ доступности сервера. [#859 #208641]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -1,25 +1,35 @@
 package ru.job4j.io;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.PrintWriter;
 
 public class Analizy {
     public static void unavailable(String source, String target) {
-        try (BufferedReader in = new BufferedReader(new FileReader(source))) {
-            PrintWriter out = new PrintWriter(new FileOutputStream(target));
+        try (BufferedReader in = new BufferedReader(new FileReader(source));
+             PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
             StringBuilder builder = new StringBuilder();
-            while (in.readLine() != null) {
-                String[] split = in.readLine().split(" ");
-                if (builder.length() == 0 && (split[0].equals("400") || split[0].equals("500"))) {
-                    out.append(split[1]);
+            String s = in.readLine();
+            while (s != null) {
+                String[] split = s.split(" ");
+                if (builder.length() == 0 && split[0].contains("400") || split[0].contains("500")) {
+                    builder.append(split[1]).append(";");
                 }
+                if (builder.length() != 0 && split[0].contains("200") || split[0].contains("300")) {
+                    builder.append(split[1]).append(";").append(System.lineSeparator());
+
+                }
+                s = in.readLine();
             }
+            out.append(builder);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
-        unavailable("source.csv", "target.csv");
+        unavailable("source.log", "target.log");
         try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
             out.println("15:01:30;15:02:32");
             out.println("15:10:30;23:12:32");
