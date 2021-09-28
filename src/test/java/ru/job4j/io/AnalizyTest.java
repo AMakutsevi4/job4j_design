78b72e25800069a70ea98b3a94241e468c9189af
AMakutsevi4 <amakutsevich@bk.ru>
2021-09-28 14:22:37 +0900

    3.0. Тестирование IO [#173905]

---
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.io;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.*;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class AnalizyTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void whenServerCrashed() throws IOException {
+        File source = folder.newFile("source.log");
+        File target = folder.newFile("target.log");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01");
+            out.println("500 10:57:01");
+            out.println("400 10:58:01");
+            out.println("200 10:59:01");
+            out.println("500 11:01:02");
+            out.println("200 11:02:03");
+        }
+        Analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(rsl::append);
+        }
+        assertThat(rsl.toString(), is("10:57:01;10:59:01;11:01:02;11:02:03;"));
+    }
+}
