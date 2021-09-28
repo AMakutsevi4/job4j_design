313509478f5eddd7624c5624f52314ff10607c98
AMakutsevi4 <amakutsevich@bk.ru>
2021-09-28 16:03:30 +0900

    4.0. File [#252491]

---
+++ src/main/java/ru/job4j/io/Dir.java
@@ -0,0 +1,19 @@
+package ru.job4j.io;
+
+import java.io.File;
+
+public class Dir {
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\MakutsevichAI\\IdeaProjects\\job4j_design4");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        System.out.println(String.format("size : %s", file.getTotalSpace()));
+        for (File subfile : file.listFiles()) {
+            System.out.println(subfile.getAbsoluteFile() + " - " + subfile.length() + " byte");
+        }
+    }
+}
