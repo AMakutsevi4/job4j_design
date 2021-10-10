6dfa85528bae03d95db49f7e9124efba8098fe42
Alexandr <amakutsevich@bk.ru>
2021-10-10 14:45:14 +0900

    5. Валидация параметров запуска. [#246865 #210853]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -3,28 +3,37 @@ package ru.job4j.io;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.function.Predicate;
 
 public class Search {
+    private static Path file;
+    private static String index;
 
     public static void main(String[] args) throws IOException {
-        if (args.length != 2) {
-            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
-        }
-        String index = args[0];
-        String format = args[1];
-        if (index == null || format == null) {
-            throw new IllegalArgumentException("Parameters are incorrect");
-        }
-        Path start = Paths.get(index);
-        search(start, p -> p.toFile().getName().endsWith(format)).forEach(System.out::println);
+        Search validateSearch = new Search();
+        validateSearch.validate(args);
+        System.out.println("Start directory :" + args[0] + "File ends with :" + args[1]);
+        search(file, p -> p.toFile().getName().endsWith(index)).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    private void validate(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        }
+        file = Path.of(args[0]);
+        if (!file.toFile().exists()) {
+            throw new IllegalArgumentException(file + "Not exist %s");
+        }
+        if (!file.toFile().isDirectory()) {
+            throw new IllegalArgumentException(file + "Not directory %s");
+        }
+        index = args[1];
     }
 }
