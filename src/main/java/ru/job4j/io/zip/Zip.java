0973324514aa6a5a408ec70788ea7c5952685f65
AMakutsevi4 <amakutsevich@bk.ru>
2021-10-21 10:50:22 +0900

    5.2. Архивировать проект [#861 #214685]

--- src/main/java/ru/job4j/io/zip/Zip.java
+++ src/main/java/ru/job4j/io/zip/Zip.java
@@ -1,4 +1,6 @@
-package ru.job4j.io;
+package ru.job4j.io.zip;
+
+import ru.job4j.io.ArgsName;
 
 import java.io.*;
 import java.nio.file.Files;
@@ -10,14 +12,28 @@ import java.util.zip.ZipOutputStream;
 
 import static ru.job4j.io.Search.search;
 
+/**
+ * Параметры для архивирования
+ * -d - directory                                       -e - exclude -o - output
+ * -d=C:\Users\MakutsevichAI\IdeaProjects\job4j_design4 -e=class -o=project.zip
+ */
 public class Zip {
-    private static String format;
-    private static Path obj;
-    private static List<File> list;
+    public static String format;
+    public static Path obj;
+    public static List<File> list;
+    public static Path path;
+
+    public static void param(String[] args) {
+        ArgsName argsName = ArgsName.of(args);
+        path = Path.of(argsName.get("d"));
+        format = argsName.get("e");
+        obj = Path.of(argsName.get("o"));
+    }
 
     public static void packFiles(List<File> sources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
             for (File f : sources) {
+                makeList();
                 zip.putNextEntry(new ZipEntry(f.getPath()));
                 try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(f))) {
                     zip.write(out.readAllBytes());
@@ -28,28 +44,26 @@ public class Zip {
         }
     }
 
-    private static void validate(String[] args) throws IllegalArgumentException {
-        if (args[0].isEmpty() || args[1].isEmpty() || args[2].isEmpty()) {
-            throw new IllegalArgumentException("Отсутствуют параметры.");
-        }
-        ArgsName argsName = ArgsName.of(args);
-        format = argsName.get("e");
-        obj = Path.of(argsName.get("o"));
-    }
-
-    private static void makeList(String[] args) throws IOException {
-        ArgsName argsName = ArgsName.of(args);
-        Path path = Path.of(argsName.get("d"));
+    private static void makeList() throws IOException {
         list = search(path, p -> !p.toFile().getName().endsWith(format))
                 .stream()
                 .map(Path::toFile)
                 .collect(Collectors.toList());
     }
 
+    private static void validate(String[] args) throws IllegalArgumentException {
+        if (args.length != 3) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        }
+        if (!Files.isDirectory(Path.of(String.valueOf(path)))) {
+            throw new IllegalArgumentException("Отсутствуюет директория. " + (args[0]));
+        }
+    }
+
     public static void main(String[] args) throws IOException {
+        param(args);
         validate(args);
         System.out.println("Архивирование...");
-        makeList(args);
         packFiles(list, new File(args[2].split("=")[1]));
         if (Files.exists(obj) && obj.toFile().length() > 0) {
             System.out.println("успешно завершено: " + obj + " " + obj.toFile().length() / 1000000 + " мегабайт");
