7ba3cd660866909e830067b2d25f6c4ef8ebbeb8
AMakutsevi4 <amakutsevich@bk.ru>
2021-10-25 15:39:34 +0900

    4. Slf4j - вывод exception. [#268853]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -4,8 +4,16 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import org.apache.log4j.BasicConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class EchoServer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+
     public static void main(String[] args) throws IOException {
+        BasicConfigurator.configure();
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
@@ -14,22 +22,25 @@ public class EchoServer {
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
+                        LOG.info(str);
                         System.out.println(str);
                         if (str.contains("Exit")) {
                             out.write(" See you later".getBytes());
                             server.close();
                             break;
                         }
-                         if (str.contains("Hello")) {
+                        if (str.contains("Hello")) {
                             out.write(" Hello, what do you say?".getBytes());
-                         } else {
-                             out.write(" What?".getBytes());
-                         }
+                        } else {
+                            out.write(" What?".getBytes());
+                        }
                         break;
                     }
                     out.flush();
                 }
             }
+        } catch (Exception e) {
+            LOG.error("Exception in log example", e);
         }
     }
 }
