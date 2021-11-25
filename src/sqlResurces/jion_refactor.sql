231723c08053738c4a21a16e8451ccddb44ed671
Alexandr <amakutsevich@bk.ru>
2021-11-25 22:14:43 +0900

    0. Join

--- src/sqlResurces/jion_refactor.sql
+++ src/sqlResurces/jion_refactor.sql
@@ -7,6 +7,6 @@ insert into emploers(name, departaments_id) values ('Александр',1);
 insert into emploers(name, departaments_id) values ('Петр',2);
 insert into emploers(name, departaments_id) values ('Вася',null);
 
-select * from emploers e left join departaments d on e.departaments_id = d.id
-union
-select * from emploers e right join departaments d on e.departaments_id = d.id;
+
+select * from emploers e left join departaments d on e.departaments_id = d.id;
+select * from departaments d right join emploers e on d.id = e.departaments_id;
