6a9ada0d50540fa4441229827886aa1c0d74beb9
Alexandr <amakutsevich@bk.ru>
2021-11-26 17:43:47 +0900

    0. Join

--- src/sqlResurces/jion_refactor.sql
+++ src/sqlResurces/jion_refactor.sql
@@ -1,12 +1,12 @@
-
-insert into departaments(name) values('Отдел продаж');
-insert into departaments(name) values('Отдел логистики');
-
-
 insert into emploers(name, departaments_id) values ('Александр',1);
 insert into emploers(name, departaments_id) values ('Петр',2);
 insert into emploers(name, departaments_id) values ('Вася',null);
 
+insert into departaments(name) values('Отдел продаж');
+insert into departaments(name) values('Отдел логистики');
 
 select * from emploers e left join departaments d on e.departaments_id = d.id;
 select * from departaments d right join emploers e on d.id = e.departaments_id;
+
+select * from departaments d left join emploers e on d.id = e.departaments_id;
+select * from emploers e right join departaments d on e.departaments_id = d.id;
