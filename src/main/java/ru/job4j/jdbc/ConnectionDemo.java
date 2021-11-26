bf02e25f00d5f5202ce8a4fd0a881ba0170ec493
Alexandr <amakutsevich@bk.ru>
2021-11-26 16:02:06 +0900

    0. JDBC

--- src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -8,17 +8,17 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionDemo {
-          public static void main(String[] args) throws ClassNotFoundException, SQLException {
-              Class.forName("org.postgresql.Driver");
-              Config cfg = new Config("./src/sqlResurces/app.properties");
-              cfg.load();
-              String url = cfg.value("url");
-              String login = cfg.value("username");
-              String password = cfg.value("password");
-              try (Connection connection = DriverManager.getConnection(url, login, password)) {
-                  DatabaseMetaData metaData = connection.getMetaData();
-                  System.out.println(metaData.getUserName());
-                  System.out.println(metaData.getURL());
-              }
-          }
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Config cfg = new Config("./src/sqlResurces/app.properties");
+        cfg.load();
+        String url = cfg.value("url");
+        String login = cfg.value("username");
+        String password = cfg.value("password");
+        try (Connection connection = DriverManager.getConnection(url, login, password)) {
+            Class.forName("org.postgresql.Driver");
+            DatabaseMetaData metaData = connection.getMetaData();
+            System.out.println(metaData.getUserName());
+            System.out.println(metaData.getURL());
         }
+    }
+}
