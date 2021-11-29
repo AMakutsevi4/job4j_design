387685c7a171721840809b3ee162822cebfda3dc
Alexandr <amakutsevich@bk.ru>
2021-11-29 16:49:15 +0900

    0.2. PrepareStatement

---
+++ src/main/java/ru/job4j/jdbc/prepareStatement/ImportDB.java
@@ -0,0 +1,69 @@
+package ru.job4j.jdbc.prepareStatement;
+
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class ImportDB {
+
+    private Properties cfg;
+    private String dump;
+
+    public ImportDB(Properties cfg, String dump) {
+        this.cfg = cfg;
+        this.dump = dump;
+    }
+
+    public List<User> load() throws IOException {
+        List<User> users = new ArrayList<>();
+        try (BufferedReader rd = new BufferedReader(new FileReader(dump))) {
+            while (rd.ready()) {
+                String s = rd.readLine();
+                users.add(new User(s.split(";")[0], s.split(";")[1]));
+            }
+        }
+        return users;
+    }
+
+    public void save(List<User> users) throws ClassNotFoundException, SQLException {
+        Class.forName(cfg.getProperty("driver"));
+        try (Connection cnt = DriverManager.getConnection(
+                cfg.getProperty("url"),
+                cfg.getProperty("username"),
+                cfg.getProperty("password")
+        )) {
+            for (User user : users) {
+                try (PreparedStatement ps = cnt.prepareStatement("insert into users(name, email) values (?, ?);")) {
+                    ps.setString(1, user.name);
+                    ps.setString(2, user.email);
+                    ps.execute();
+                }
+            }
+        }
+    }
+
+    private static class User {
+        String name;
+        String email;
+
+        public User(String name, String email) {
+            this.name = name;
+            this.email = email;
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        Properties cfg = new Properties();
+        try (FileInputStream in = new FileInputStream("./src/sqlResurces/spammer.properties")) {
+            cfg.load(in);
+        }
+        ImportDB db = new ImportDB(cfg, "./src/main/java/ru/job4j/jdbc/prepareStatement/dump.txt");
+        db.save(db.load());
+    }
+}
---
+++ src/sqlResurces/spammer.sql
@@ -0,0 +1,5 @@
+create table users (
+id serial primary key,
+name varchar(65) not null,
+email varchar(65) not null
+);
