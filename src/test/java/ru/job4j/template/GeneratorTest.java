20a66b6ac0f7e0e6bb6073e5b9d3ec2d576bfcf7
AMakutsevi4 <amakutsevich@bk.ru>
2022-01-14 13:56:11 +0900

    3. Шаблонизатор.

---
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,47 @@
+package ru.job4j.template;
+
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class GeneratorTest {
+
+    @Test
+    public void whenTemplateThenWork() {
+        String test = "I am a ${name}, Who are ${subject}? ";
+        Map<String, String> map = Map.of("name", "Alexandr Makutsevich", "subject", "you");
+        TextGenerator textGenerator = new TextGenerator();
+        textGenerator.produce(test, map);
+        String wait = "I am a Alexandr Makutsevich, Who are you? ";
+        assertThat(textGenerator, is(textGenerator));
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenTemplateThenMoreKeys() {
+        String test = "I am a ${name}, Who are ${subject}? ";
+        Map<String, String> map = Map.of("name", "Alexandr Makutsevich", "subject", "you", "Age", "30");
+        TextGenerator textGenerator = new TextGenerator();
+        textGenerator.produce(test, map);
+        assertThat(test, is(textGenerator));
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenTemplateThenNoKeys() {
+        String test = "I am a ${name}, Who are ${subject}? ";
+        Map<String, String> map = Map.of("name", "Alexandr Makutsevich");
+        TextGenerator textGenerator = new TextGenerator();
+        textGenerator.produce(test, map);
+        assertThat(test, is(textGenerator));
+    }
+}
