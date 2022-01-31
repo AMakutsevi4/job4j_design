12c5e0859396d5e3aa52e00a0ba1f1ba641e21d7
Alexandr <amakutsevich@bk.ru>
2022-01-31 22:42:01 +0900

    1. Поддержка форматом XML, JSON в генераторе отчетов.

--- src/test/java/ru/job4j/design/srp/ReportJSONTest.java
+++ src/test/java/ru/job4j/design/srp/ReportJSONTest.java
@@ -4,8 +4,6 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.junit.Test;
 
-import java.util.Calendar;
-
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -14,14 +12,14 @@ public class ReportJSONTest {
     @Test
     public void whenSupportJsonThenBooker() {
         MemStore store = new MemStore();
-        Calendar now = Calendar.getInstance();
-        Employee worker = new Employee("Ivan", now, now, 100);
+        Employee worker = new Employee("Ivan", null, null, 100);
         store.add(worker);
         Report engine = new ReportBooker(store);
         SerializationSON json = new SerializationSON();
         final Gson gson = new GsonBuilder().create();
         String strJson = json.generate(engine.generate(e -> true));
         String rsl = gson.fromJson(strJson, String.class);
-        assertThat(rsl, is(engine.generate(em -> true)));
+        String exp = "Name; Hired; Fired; Salary;\r\nIvan;null;null;7500.0;\r\n";
+        assertThat(rsl, is(exp));
     }
 }
