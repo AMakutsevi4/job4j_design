2557e64b63f83a908ff1672d590740fc9e0966f8
AMakutsevi4 <amakutsevich@bk.ru>
2022-09-19 15:37:28 +0900

    task 2.6

---
+++ src/main/java/ru/job4j/stream/CollectToAnyMap.java
@@ -0,0 +1,14 @@
+package ru.job4j.stream;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CollectToAnyMap {
+    public static Map<Integer, Integer> collect(Stream<Integer> data) {
+        return data.
+                collect(Collectors.toMap(k -> k, v -> v * v,
+                        (integer, integer2) -> integer, LinkedHashMap::new));
+    }
+}
