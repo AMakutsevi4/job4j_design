33954e5ee548d4a67b044ee284e41b120d6cc268
AMakutsevi4 <amakutsevich@bk.ru>
2022-09-23 12:51:25 +0900

    task 9.5, 9.6, 9.7

---
+++ src/main/java/ru/job4j/stream/OptionalStream.java
@@ -0,0 +1,51 @@
+package ru.job4j.stream;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OptionalStream {
+
+    public static class PhoneNumber {
+
+        private String phone;
+
+        public PhoneNumber(String phone) {
+            this.phone = phone;
+        }
+
+        public String getPhone() {
+            return phone;
+        }
+
+    }
+
+    public static class User {
+
+        private int id;
+
+        private List<PhoneNumber> numbers;
+
+        public User(int id, List<PhoneNumber> numbers) {
+            this.id = id;
+            this.numbers = numbers;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public List<PhoneNumber> getNumbers() {
+            return numbers;
+        }
+    }
+
+    public static List<PhoneNumber> collectNumber(List<User> users, int id, String region) {
+        return users.stream()
+                .filter(user -> user.getId() == id) // фильтруем все по id
+                .flatMap(user -> user.getNumbers().stream()) // создаем поток фильрованных элементов
+                .filter(user -> user.getPhone().startsWith(region)) // фильтруем номера которые начинаются с region
+                .collect(Collectors.toList());
+    }
+}
