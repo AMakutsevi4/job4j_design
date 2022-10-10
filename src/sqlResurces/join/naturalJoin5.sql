1137e68e40e44a9f98df9fbca8b612e6a416a1f3
AMakutsevi4 <amakutsevich@bk.ru>
2022-10-10 09:51:35 +0900

    natural join 4, 5

---
+++ src/sqlResurces/join/naturalJoin5.sql
@@ -0,0 +1,27 @@
+CREATE TABLE departments (
+                             department_id int primary key,
+                             name text not null
+);
+
+CREATE TABLE employees (
+                           id int primary key,
+                           name text,
+                           department_id int references departments(department_id)
+);
+
+INSERT INTO departments VALUES (1, 'Sales'),
+                               (2, 'Marketing'),
+                               (3, 'HR'),
+                               (4, 'IT'),
+                               (5, 'Production');
+
+INSERT INTO employees VALUES (1, 'Ivan Ivanov', 1),
+                             (2, 'Petr Petrov', 1),
+                             (3, 'Olga Sergeeva', 2),
+                             (4, 'Michael Shnurov', 3),
+                             (5, 'Irina Trubkina', 4),
+                             (6, 'Evgenii Shtukov', null);
+
+
+select * from departments
+natural join employees;
