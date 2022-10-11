550271c495010b10eff1817f1e6aa51995621a0b
AMakutsevi4 <amakutsevich@bk.ru>
2022-10-11 12:10:50 +0900

    where avg, sum 2, 3, 1

---
+++ src/sqlResurces/select/avg3.sql
@@ -0,0 +1,22 @@
+CREATE TABLE transactions(
+                             id int not null primary key,
+                             purpose text,
+                             client_id int,
+                             amount int,
+                             payment_date timestamp
+);
+
+INSERT INTO transactions VALUES(1, 'Credit', 1, 132, '2022-06-01 12:00:00');
+INSERT INTO transactions VALUES(2, 'Current payment', 2, 94, '2022-06-01 12:00:00');
+INSERT INTO transactions VALUES(3, 'Service', 3, 194, '2022-06-01 12:00:00');
+INSERT INTO transactions VALUES(4, 'Vacation', 4, 254, '2022-06-01 12:00:00');
+INSERT INTO transactions VALUES(5, 'Credit', 1, 244, '2022-06-01 12:00:00');
+INSERT INTO transactions VALUES(6, 'Current payment', 2, 114, '2022-06-01 12:00:00');
+INSERT INTO transactions VALUES(7, 'Service', 3, 178, '2022-06-01 12:00:00');
+INSERT INTO transactions VALUES(8, 'Vacation', 4, 196, '2022-06-01 12:00:00');
+INSERT INTO transactions VALUES(9, 'Credit', 1, 198, '2022-06-01 12:00:00');
+INSERT INTO transactions VALUES(10, 'Current payment', 2, 78, '2022-06-01 12:00:00');
+INSERT INTO transactions VALUES(11, 'Service', 3, 156, '2022-06-01 12:00:00');
+INSERT INTO transactions VALUES(12, 'Vacation', 4, 152, '2022-06-01 12:00:00');
+
+select purpose, avg(amount) from transactions group by purpose;
