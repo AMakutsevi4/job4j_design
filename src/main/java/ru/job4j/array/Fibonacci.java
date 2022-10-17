66e51942d2a3703aa38549976d46f28c9278ea03
AMakutsevi4 <amakutsevich@bk.ru>
2022-10-17 12:25:59 +0900

    array 51

---
+++ src/main/java/ru/job4j/array/Fibonacci.java
@@ -0,0 +1,15 @@
+package ru.job4j.array;
+
+public class Fibonacci {
+    public static boolean checkArray(int[] data) {
+        boolean rsl = true;
+        for (int i = 2; i < data.length; i++) {
+            if (data[i] != data[i - 1] + data[i - 2]) {
+                rsl = false;
+                break;
+            }
+
+        }
+        return rsl;
+    }
+}
