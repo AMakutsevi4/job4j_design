07b646c5106af074e0576ca210fbe7a66b6af517
AMakutsevi4 <amakutsevich@bk.ru>
2022-10-20 17:01:59 +0900

    initials

---
+++ src/main/java/ru/job4j/array/Initials.java
@@ -0,0 +1,7 @@
+package ru.job4j.array;
+
+public class Initials {
+    public static String convert(String[] fio) {
+        return fio[0] + " " + fio[1].charAt(0) + "." + fio[2].charAt(0) + ".";
+    }
+}
