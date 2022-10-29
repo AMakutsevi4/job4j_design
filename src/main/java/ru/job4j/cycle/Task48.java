7fbfda5faaf462b7ecb9a9468b33764a57695660
Alexandr <amakutsevich@bk.ru>
2022-10-29 23:55:00 +0900

    Task48.java

---
+++ src/main/java/ru/job4j/cycle/Task48.java
@@ -0,0 +1,10 @@
+package ru.job4j.cycle;
+
+public class Task48 {
+    public static void loop(int num) {
+        for (int i = 1; i < num; i++) {
+            System.out.print(i + "+");
+        }
+        System.out.println(num);
+    }
+}
