7b12ef7487d1674104773738138154e050f81ec8
AMakutsevi4 <amakutsevich@bk.ru>
2022-12-06 14:59:21 +0900

    Task 69

---
+++ src/main/java/ru/job4j/condition/Task69.java
@@ -0,0 +1,7 @@
+package ru.job4j.condition;
+
+public class Task69 {
+    public static void squareOrRectangle(int a, int b) {
+        System.out.println(a == b ? "Квадрат" : "Прямоугольник");
+    }
+}
