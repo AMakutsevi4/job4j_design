920998511d1d4da26bbc2f7b030ade139a8c041e
AMakutsevi4 <amakutsevich@bk.ru>
2022-12-07 17:47:03 +0900

    Task 66

--- src/main/java/ru/job4j/condition/Task66.java
+++ src/main/java/ru/job4j/condition/Task66.java
@@ -2,16 +2,15 @@ package ru.job4j.condition;
 
 public class Task66 {
     public static void daytime(int number) {
-        if (number >= 6 && number <= 10) {
-            System.out.println("Утро");
-        } else if (number >= 11 && number <= 17) {
-            System.out.println("День");
-        } else if (number >= 18 && number <= 22) {
-            System.out.println("Вечер");
-        } else if (number <= 5 && number >= 0 || number == 23) {
-            System.out.println("Ночь");
-        }  else {
-            System.out.println("Не является часом");
+        String string;
+        switch (number) {
+            case 23, 0, 1, 2, 3, 4, 5 -> string = "Ночь";
+            case 6, 7, 8, 9, 10 -> string = "Утро";
+            case 11, 12, 13, 14, 15, 16, 17 -> string = "День";
+            case 18, 19, 20, 21, 22 -> string = "Вечер";
+            default -> string = "Не является часом";
         }
+        System.out.println(string);
+
     }
 }
