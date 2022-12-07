5ebe888f5b3bbe2e5f8969ab1d569075f6fceb38
AMakutsevi4 <amakutsevich@bk.ru>
2022-12-07 17:51:02 +0900

    Task 65

---
+++ src/main/java/ru/job4j/condition/Task65.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+public class Task65 {
+    public static void season(int number) {
+        String string;
+        switch (number) {
+            case 12, 1, 2 -> string = "Зима";
+            case 3, 4, 5 -> string = "Весна";
+            case 6, 7, 8 -> string = "Лето";
+            case 9, 10, 11 -> string = "Осень";
+            default -> string = "Не является месяцем";
+        }
+        System.out.println(string);
+    }
+}
