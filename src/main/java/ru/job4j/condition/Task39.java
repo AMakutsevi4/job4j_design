df7a2f69186d7615d67b66467da2394772df97f6
AMakutsevi4 <amakutsevich@bk.ru>
2022-12-08 17:27:07 +0900

    Task 39

---
+++ src/main/java/ru/job4j/condition/Task39.java
@@ -0,0 +1,7 @@
+package ru.job4j.condition;
+
+public class Task39 {
+    public static void isSymmetric(int number) {
+        System.out.println(number / 100 == number % 10 ? "Да" : "Нет");
+    }
+}
