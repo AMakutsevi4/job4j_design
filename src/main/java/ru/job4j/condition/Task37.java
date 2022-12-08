cfade2e870f0d0fce1e136f6f8fe8ef6c7697dc6
AMakutsevi4 <amakutsevich@bk.ru>
2022-12-08 18:03:00 +0900

    Task 37

---
+++ src/main/java/ru/job4j/condition/Task37.java
@@ -0,0 +1,7 @@
+package ru.job4j.condition;
+
+public class Task37 {
+    public static void numsCompare(int number) {
+        System.out.println(number / 10 > number % 10 ? "Да" : "Нет");
+    }
+}
