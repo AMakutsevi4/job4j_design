e557e0e2515368d8044234cfddc2f22233c376b7
AMakutsevi4 <amakutsevich@bk.ru>
2022-12-14 14:57:19 +0900

    Task 52

---
+++ src/main/java/ru/job4j/condition/Task52.java
@@ -0,0 +1,7 @@
+package ru.job4j.condition;
+
+public class Task52 {
+    public static void isEquals(int a1, int a2, int b1, int b2) {
+        System.out.println(a1 * b2 == a2 * b1 ? "Да" : "Нет");
+    }
+}
