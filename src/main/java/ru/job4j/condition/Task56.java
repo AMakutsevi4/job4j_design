ba0002ee6b96b787151bc7cfd26100d9d86e7b4b
AMakutsevi4 <amakutsevich@bk.ru>
2022-12-15 14:02:16 +0900

    Task 56

---
+++ src/main/java/ru/job4j/condition/Task56.java
@@ -0,0 +1,10 @@
+package ru.job4j.condition;
+
+public class Task56 {
+    public static void isTriangle(int a, int b, int c) {
+
+            System.out.println(a + b > c && a + c > b && b + c > a ? "Да" : "Нет");
+        }
+    }
+
+
