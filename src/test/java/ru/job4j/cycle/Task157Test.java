f2d7366a34ad5295f1eb182f6fea8928bcde53d1
AMakutsevi4 <amakutsevich@bk.ru>
2022-12-29 14:38:38 +0900

    Task 157

---
+++ src/test/java/ru/job4j/cycle/Task157Test.java
@@ -0,0 +1,22 @@
+package ru.job4j.cycle;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class Task157Test {
+    public String ln = System.lineSeparator();
+
+    @Test
+    public void whenTestThen6And28And496And8128() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Task157.loop();
+        String expected = "6 28 496 8128" + ln;
+        assertThat(out.toString(), is(expected));
+    }
+}
