543864300032fc947fb8022067d27f887aa0ab0a
Alexandr <amakutsevich@bk.ru>
2023-01-31 20:53:07 +0900

    Task 166

---
+++ src/main/java/ru/job4j/cycle/Task166.java
@@ -0,0 +1,27 @@
+package ru.job4j.cycle;
+
+import java.util.StringJoiner;
+
+public class Task166 {
+    public static void loop() {
+        StringJoiner joiner = new StringJoiner(" ");
+        for (int i = 100; i <= 300; i++) {
+            if (i % 10 == 0 && sumDel(i) % 10 == 0) {
+                joiner.add(String.valueOf(i));
+            }
+        }
+        System.out.println(joiner);
+    }
+
+    private static int sumDel(int num) {
+        int rsl = 0;
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0 && i % 2 !=0 ) {
+                rsl += i;
+            }
+        }
+        return rsl;
+    }
+}
+
+
