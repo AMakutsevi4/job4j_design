031867202320a5aef73a8dc75667a3b444746a09
Alexandr <amakutsevich@bk.ru>
2023-02-28 21:28:17 +0900

    update

--- src/main/java/ru/job4j/array/ReverseString.java
+++ src/main/java/ru/job4j/array/ReverseString.java
@@ -3,9 +3,9 @@ package ru.job4j.array;
 public class ReverseString {
     public static char[] reverseString(char[] s) {
         char[] test = new char[s.length];
-        int index = 0;
-        for (int i = s.length - 1; i >= 0; i--) {
-            test[index++] = s[i];
+
+        for (int i = 0; i < s.length; i++) {
+            test[i] = s[s.length - i - 1];
         }
         return test;
     }
