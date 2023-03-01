44ad28a9b25e4effd4d55db82b766f24bfd02738
Alexandr <amakutsevich@bk.ru>
2023-03-01 19:53:39 +0900

    DeleteNode

---
+++ src/main/java/ru/job4j/array/DeleteNode.java
@@ -0,0 +1,23 @@
+package ru.job4j.array;
+
+public class DeleteNode {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+
+    public void deleteNode(ListNode node) {
+//Данный связанный список будет иметь как минимум два элемента
+//Все значения узлов будут уникальными.
+//Данный узел не будет последним
+        ListNode temp = node.next;
+        node.val = node.next.val;
+        node.next = node.next.next;
+        temp.next=null;//разрывает все ссылки из переданного узла.
+    }
+}
