5d86b664d849172d20345ed8bc142e36bea0e142
AMakutsevi4 <amakutsevich@bk.ru>
2024-02-19 11:50:59 +0900

    16. Split

--- src/main/java/ru/job4j/array/Split.java
+++ src/main/java/ru/job4j/array/Split.java
@@ -4,10 +4,21 @@ import java.util.Arrays;
 
 public class Split {
     public static char[][] split(char[] str, char c) {
-        char[][] result = new char[str.length / 2][];
+
+        char[][] result = new char[str.length / 2][str.length];
+        char[] test = new char[str.length];
         int commonCount = 1;
-
-
+        int count = 0;
+        for (char value : str) {
+            if (value != c) {
+                test[count++] = value;
+            } else {
+                result[commonCount - 1] = Arrays.copyOf(test, count);
+                count = 0;
+                commonCount++;
+            }
+        }
+        result[commonCount - 1] = Arrays.copyOf(test, count);
         return Arrays.copyOf(result, commonCount);
 
     }
