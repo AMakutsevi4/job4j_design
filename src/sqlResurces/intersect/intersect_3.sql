52a28acefeccb48fc9b05dae76a07d6403ae24da
Alexandr <amakutsevich@bk.ru>
2024-08-25 22:10:06 +0700

    3. intersect

---
+++ src/sqlResurces/intersect/intersect_3.sql
@@ -0,0 +1,43 @@
+CREATE TABLE book_details3
+(
+    id   INT PRIMARY KEY,
+    name TEXT
+);
+
+CREATE TABLE top_selling_books3
+(
+    id   INT PRIMARY KEY,
+    name TEXT
+);
+
+INSERT INTO book_details3(id, name)
+VALUES (1, 'Великий Гэтсби'),
+       (2, 'Портрет Дориана Грея'),
+       (3, 'Большие надежды'),
+       (4, 'Грозовой перевал'),
+       (5, 'Бегущий за ветром'),
+       (6, 'Над пропастью во ржи'),
+       (7, 'Властелин колец'),
+       (8, 'Тёмные начала'),
+       (9, 'Убить пересмешника'),
+       (10, 'Гроздья гнева'),
+       (11, 'Франкенштейн'),
+       (12, 'Думай и богатей');
+
+INSERT INTO top_selling_books3(id, name)
+VALUES (2, 'Портрет Дориана Грея'),
+       (3, 'Большие надежды'),
+       (4, 'Маленькие женщины'),
+       (5, 'Паутина Шарлотты'),
+       (6, 'Над пропастью во ржи'),
+       (7, 'Властелин колец'),
+       (10, 'Гроздья гнева'),
+       (11, 'Франкенштейн'),
+       (12, 'Думай и богатей');
+
+SELECT name
+FROM book_details3
+INTERSECT
+SELECT name
+FROM top_selling_books3
+ORDER BY name;
